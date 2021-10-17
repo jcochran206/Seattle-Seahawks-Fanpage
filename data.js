3edaac7534708cf903e078f72ed8f4a60b97d950
jcochran206 <jonathan.o.cochran@gmail.com>
2021-10-17 14:27:39 -0700

    added nav style and hero section

--- data.js
+++ data.js
@@ -4,25 +4,26 @@ const team = {
     'loses' : 3,
 }
 
-const game_week = {
+const game_week = [{
 "uid": "wk1",
 "week": "Week one",
 "homeTeam": "Indianapolis Colts",
 "awayTeam": "Seattle Seahawks",
 "homeTeamScore": 16, 
 "awayTeamScore": 28,
-'result': 'W',
+'result': 'W'},
 
-"uid": "wk2",
+{"uid": "wk2",
 "week": "Week two",
 "homeTeam": "Seattle Seahawks",
 "awayTeam": "Tennesse Titans",
 "homeTeamScore": 30, 
 "awayTeamScore": 33,
 'result': 'L',     
-}
+}]
 
-const players = {
+const players = [
+    {
     'uid' : 'rw1',
     'Name' : 'Russell Wilson',
     'Pos' : 'QB',
@@ -31,8 +32,10 @@ const players = {
     'age': 32,
     'height':'5-11',
     'weight': '215',
-    'photo' : 'img/russ.jpeg',
+    'photo' : 'img/russ.jpeg'
+    },
 
+    {
     'uid': 'bw1',
     'Name' : 'Bobby Wagner',
     'Pos' : 'QB',
@@ -42,4 +45,6 @@ const players = {
     'height':'6-0',
     'weight': '242',
     'photo' : 'img/bobby.jpeg'
-}
+    }
+
+]
