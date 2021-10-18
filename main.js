fce0c70404df952b3f4cc74b1ca03b72b6def805
jcochran206 <jonathan.o.cochran@gmail.com>
2021-10-17 18:17:02 -0700

    added effects and nav function

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+//scroll effect
+const nav = document.querySelector('.nav');
+window.addEventListener('scroll', fixNav);
+
+function fixNav() {
+    console.log('window', window.scrollY);
+    console.log('offset', nav.offsetHeight);
+    if(window.scrollY > nav.offsetHeight - 100){
+        nav.classList.add('active')
+    }else {
+        nav.classList.remove('active')
+    }
+}
+
+
 //obtain team status
 //function loop players data
 //function to calculate win percentage
